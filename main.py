d82e019237fb08325053ed5de2585fe09897361d
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-22 14:52:28 +0000

    ccc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #ejercicio 1
+print("Ejercicio 1:")
 import math
 print(math.pi)
 def area_circulo(radio):
@@ -8,3 +9,21 @@ print("El área de su circulo es:", (area_circulo(radio)))
 print("")
 
 #ejercicio 2 
+print("Ejercicio 2:")
+def mayor(lista):
+  return max(lista)
+def lee_numero():
+  lista=[]
+  for i in range (0,3):
+    numero= int(input(f"Introduce el número{i}:"))
+    lista.append(numero)
+  return mayor(lista)
+print(lee_numero())
+print("")
+
+#ejercicio 3
+print("Ejercicio 3:")
+def imc(peso, altura): 
+  imc= peso / (altura*altura)
+  if imc<18.5:
+  
